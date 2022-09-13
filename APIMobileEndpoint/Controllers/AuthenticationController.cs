86c643cac1594ca1a6db2edd162eae272c8688bc
MohammadouSadjo <mohammadou.sadjo@2023.ucac-icam.com>
2022-09-13 15:24:00 +0100

    Login

---
+++ APIMobileEndpoint/Controllers/AuthenticationController.cs
@@ -0,0 +1,70 @@
+﻿using APIMobileEndpoint.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+
+namespace APIMobileEndpoint.Controllers
+{
+    [ApiController]
+    [Route("authentication")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult PostAuthentication([FromBody] LoginInformations loginInfo)
+        {
+            string query = "select * from clients where login='"+ loginInfo.login +"'";
+            string truePassword;
+            User user = new User();
+
+            string uid = "root";
+            string pwd = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + pwd;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    myReader.Read();
+
+                    if (!myReader.HasRows)
+                    {
+                        myReader.Close();
+                        mycon.Close();
+                        return NotFound("Not Found");
+                    }
+                    else
+                    {
+                        truePassword = (string)myReader["mot_de_passe"];
+
+                        if (truePassword != loginInfo.password)
+                        {
+                            myReader.Close();
+                            mycon.Close();
+                            return NotFound("Not Found");
+                        }
+                        else
+                        {
+                            user.id = (int)myReader["id_client"];
+                            user.nom = (string)myReader["nom"];
+                            user.prenom = (string)myReader["prenom"];
+                            user.email = (string)myReader["email"];
+
+                            myReader.Close();
+                            mycon.Close();
+                            return new JsonResult(user);
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ APIMobileEndpoint/Models/LoginInformations.cs
@@ -0,0 +1,8 @@
+﻿namespace APIMobileEndpoint.Models
+{
+    public class LoginInformations
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
