47c4d0c46075409b39539cebf9218d22c2000ae4
MohammadouSadjo <mohammadou.sadjo@2023.ucac-icam.com>
2022-09-14 11:54:09 +0100

    Update Get intervenants

---
+++ APIMobileEndpoint/Controllers/IntervenantController.cs
@@ -0,0 +1,53 @@
+﻿using APIMobileEndpoint.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace APIMobileEndpoint.Controllers
+{
+    [ApiController]
+    [Route("intervenant")]
+    public class IntervenantController : Controller
+    {
+        [HttpGet("getintervenants")]
+        public IActionResult GetIntervenant()
+        {
+            string query = "select * from intervenants";
+            List<Intervenant> listIntervenant = new List<Intervenant>();
+
+            string uid = "root";
+            string pwd = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + pwd;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    while (myReader.Read())
+                    {
+                        listIntervenant.Add(new Intervenant()
+                        {
+                            id = (int)myReader.GetValue(0),
+                            nom = (string)myReader.GetValue(1),
+                            prenom = (string)myReader.GetValue(2),
+                            telephone = (string)myReader.GetValue(3),
+                            email = (string)myReader.GetValue(4)
+                        });
+                    }
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(listIntervenant);
+        }
+    }
+}
---
+++ APIMobileEndpoint/Models/Intervenant.cs
@@ -0,0 +1,11 @@
+﻿namespace APIMobileEndpoint.Models
+{
+    public class Intervenant
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string telephone { get; set; }
+        public string email { get; set; }
+    }
+}
