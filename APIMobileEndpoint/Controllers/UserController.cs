a07158e842edff78571de1e9616926b32e383349
MohammadouSadjo <mohammadou.sadjo@2023.ucac-icam.com>
2022-09-14 14:37:21 +0100

    Uodate add user

--- APIMobileEndpoint/Controllers/UserController.cs
+++ APIMobileEndpoint/Controllers/UserController.cs
@@ -92,5 +92,33 @@ namespace APIMobileEndpoint.Controllers
             return new JsonResult(listUser);
         }
 
+        [HttpPost("adduser")]
+        public IActionResult AddUser([FromBody] User user)
+        {
+
+            string query = "insert into clients(nom,prenom,login,mot_de_passe,email) values('" + user.nom + "', '" + user.prenom + "', '" + user.login + "', '" + user.mot_de_passe + "','"+ user.email +"')";
+
+            string uid = "root";
+            string password = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + password;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(user);
+        }
+
     }
 }
--- APIMobileEndpoint/Models/User.cs
+++ APIMobileEndpoint/Models/User.cs
@@ -7,6 +7,8 @@ namespace APIMobileEndpoint.Models
         public int id { get; set; }
         public string nom { get; set; }
         public string prenom { get; set; }
+        public string login { get; set; }
+        public string mot_de_passe { get; set; }
         public string email { get; set; }
         public string role { get; set; }
     }
