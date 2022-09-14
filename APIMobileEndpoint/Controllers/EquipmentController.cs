2fdb0fe5a202856a28135f8abb39e07cb7d0372d
MohammadouSadjo <mohammadou.sadjo@2023.ucac-icam.com>
2022-09-14 11:27:38 +0100

    Update get all equipments, update equipment, delete equipment

--- APIMobileEndpoint/Controllers/EquipmentController.cs
+++ APIMobileEndpoint/Controllers/EquipmentController.cs
@@ -50,6 +50,46 @@ namespace APIMobileEndpoint.Controllers
             return new JsonResult(listEquipment);
         }
 
+        [HttpGet("getequipments")]
+        public IActionResult GetEquipments()
+        {
+            string query = "select * from equipements";
+            List<Equipment> listEquipment = new List<Equipment>();
+
+            string uid = "root";
+            string pwd = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + pwd;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    while (myReader.Read())
+                    {
+                        listEquipment.Add(new Equipment()
+                        {
+                            id = (int)myReader.GetValue(0),
+                            clientId = (int)myReader.GetValue(1),
+                            nom = (string)myReader.GetValue(2),
+                            description = (string)myReader.GetValue(3)
+                        });
+                    }
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(listEquipment);
+        }
+
         [HttpGet("getpositionandmovementtime/{equipmentId}")]
         public IActionResult GetPositionAndMovementTime(int equipmentId)
         {
@@ -156,6 +196,61 @@ namespace APIMobileEndpoint.Controllers
             }
 
             return new JsonResult(equipment);
+        }
+
+        [HttpPut("updateequipment/{equipmentId}")]
+        public IActionResult UpdateEquipment([FromBody] Equipment equipment, int equipmentId)
+        {
+            string query = "update equipements set client_id='"+ equipment.clientId +"',nom='"+ equipment.nom +"',description='"+ equipment.description +"' where id_equipement='"+ equipmentId +"'";
+
+            string uid = "root";
+            string password = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + password;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            equipment.id = equipmentId;
+            return new JsonResult(equipment);
+        }
+
+        [HttpDelete("deleteequipment/{equipmentId}")]
+        public IActionResult DeleteEquipment(int equipmentId)
+        {
+            string query = "delete from equipements where id_equipement='" + equipmentId + "'";
+
+            string uid = "root";
+            string password = "";
+            string sqlDataSource = "SERVER=localhost;PORT=3306;" +
+                 "DATABASE=agrotech;" +
+                 "UID=" + uid + ";PASSWORD=" + password;
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted");
         }
     }
 
